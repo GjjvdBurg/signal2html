9c6449df7df3c6e08ba4c9d0a89e566885fe55e8
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2020-06-12 23:49:03 +0100

    Significant improvements to styling and output

--- signalbrowse/html.py
+++ signalbrowse/html.py
@@ -5,10 +5,18 @@
 """
 
 import os
+import datetime as dt
 
 from jinja2 import Environment, PackageLoader, select_autoescape
 
-from .types import get_named_message_type, is_joined_type
+from .models import MMSMessageRecord
+from .types import (
+    get_named_message_type,
+    is_joined_type,
+    is_outgoing_call,
+    is_missed_call,
+    is_incoming_call,
+)
 
 
 def dump_thread(thread, output_dir):
@@ -23,15 +31,49 @@ def dump_thread(thread, output_dir):
     template = env.get_template("thread.html")
 
     thread_name = thread.recipient.name[0].strip()
+    is_group = False
+    if thread.recipient.recipientId._id.startswith('__textsecure_group__'):
+        is_group = True
+
+    prev_date = None
 
     simple_messages = []
     for msg in messages:
         if is_joined_type(msg._type):
             continue
+
+        date_sent = dt.datetime.fromtimestamp(msg.dateSent // 1000)
+        date_sent = date_sent.replace(microsecond=(msg.dateSent % 1000) * 1000)
+        if prev_date is None or date_sent.date() != prev_date:
+            prev_date = date_sent.date()
+            out = {
+                "date_msg": True,
+                "body": date_sent.strftime("%a, %b %d, %Y"),
+            }
+            simple_messages.append(out)
+
+        isCall = False
+        if is_incoming_call(msg._type):
+            isCall = True
+            msg.body = f"{thread_name} called you"
+        elif is_outgoing_call(msg._type):
+            isCall = True
+            msg.body = "You called"
+        elif is_missed_call(msg._type):
+            isCall = True
+            msg.body = "Missed call"
+
         out = {
+            "isCall": isCall,
             "type": get_named_message_type(msg._type),
             "body": "" if msg.body is None else msg.body,
+            "date": date_sent,
+            "attachments": [],
+            "id": msg._id,
         }
+
+        if isinstance(msg, MMSMessageRecord):
+            out["attachments"] = msg.attachments
         simple_messages.append(out)
 
     if not simple_messages:
@@ -39,8 +81,9 @@ def dump_thread(thread, output_dir):
 
     html = template.render(thread_name=thread_name, messages=simple_messages)
 
-    filename = os.path.join(output_dir, thread_name.replace(' ', '_') + 
-            ".html")
+    filename = os.path.join(
+        output_dir, thread_name.replace(" ", "_") + ".html"
+    )
     with open(filename, "w") as fp:
         fp.write(html)
 
