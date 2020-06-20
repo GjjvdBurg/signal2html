678d07c0cf8a3516259185a477994441cf478c44
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2020-06-20 14:34:45 +0100

    add version flag to cli

--- signal2html/ui.py
+++ signal2html/ui.py
@@ -6,15 +6,25 @@
 
 import argparse
 
+from . import __version__
 from .core import process_backup
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input-dir", help="Input directory", 
-            required=True)
-    parser.add_argument("-o", "--output-dir", help="Output directory", 
-            required=True)
+    parser.add_argument(
+        "-i", "--input-dir", help="Input directory", required=True
+    )
+    parser.add_argument(
+        "-o", "--output-dir", help="Output directory", required=True
+    )
+    parser.add_argument(
+        "-V",
+        "--version",
+        help="Show version and exit",
+        action="version",
+        version=__version__,
+    )
     return parser.parse_args()
 
 
